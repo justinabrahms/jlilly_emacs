841ea82e536dbe3685f464faa1d868f00d21ba76
Justin Lilly <justinlilly@gmail.com>
2008-10-20 14:38:12 -0400

    Initial import of orgmode settings

---
+++ configs/cfg_orgmode.el
@@ -0,0 +1,11 @@
+;; Configurations for orgmode
+(setq org-hide-leading-stars t)
+(setq org-return-follows-link t)
+;; The abbrev list allows me to insert links like
+;; [[foo:google]]
+;; which will google for "foo"
+(setq org-link-abbrev-alist
+      '(("google"  . "http://www.google.com/search?q=")))
+(setq org-todo-keywords '("TODO" "WAITING" "IN PROGRESS" "DONE")
+      org-todo-interpretation 'sequence)
+(setq org-log-done t) ;; timestamp when things are marked completed
