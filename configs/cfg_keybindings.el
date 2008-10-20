66a43f9690af9c3a7f3656fa421f1cd832750b05
Justin Lilly <justinlilly@gmail.com>
2008-10-20 14:32:13 -0400

    Testing out using C-xC-m instead of M-x

--- configs/cfg_keybindings.el
+++ configs/cfg_keybindings.el
@@ -4,3 +4,5 @@
 (global-set-key [(control tab)] 'ido-switch-buffer)
 
 (global-set-key [(control shift a)] 'mark-whole-buffer)
+(global-set-key "\C-x\C-m" 'execute-extended-command) ;; provides an alternative to M-x
+(global-set-key "\C-c\C-m" 'execute-extended-command) ;; ditto
