45e12a061b6c132965b0d07c07c5e523039cfe70
Justin Lilly <justinlilly@gmail.com>
2008-10-20 02:40:15 -0400

    Added helper options to ido config and added an open recently closed files command (meta f11)

--- configs/cfg_ido.el
+++ configs/cfg_ido.el
@@ -1,6 +1,11 @@
 ;; IDO Rules!
 (require 'ido)
 (ido-mode t)
+(ido-everywhere t)
+(setq ido-enable-flex-matching t) ;; pyf will match pythonfile
+(setq ido-use-filename-at-point t)
+(setq ido-auto-merge-work-directories-length -1)
+
 (global-set-key [(control tab)] 'ido-switch-buffer)
 
 (custom-set-variables
@@ -10,3 +15,11 @@
 (custom-set-faces
         '(ido-only-match ((((class color)) (:foreground "Blue"))))
         '(ido-first-match ((t (:background "Blue" :foreground "white")))))
+
+(require 'recentf)
+(setq recentf-max-saved-items 100)
+(defun ido-choose-from-recentf ()
+  "Use ido to select a recently opened file from the `recentf-list`"
+  (interactive)
+  (find-file (ido-completing-read "Open file: " recentf-list nil t)))
+(global-set-key [(meta f11)] 'ido-choose-from-recent)
