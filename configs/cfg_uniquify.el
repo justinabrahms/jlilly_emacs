9905aa6750a99befef368fa1421f46302513d970
Justin Lilly <justinlilly@gmail.com>
2008-10-22 09:48:07 -0400

    initial import of the uniquify configuration via _newman_

---
+++ configs/cfg_uniquify.el
@@ -0,0 +1,5 @@
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'reverse)
+(setq uniquify-separator "/")
+(setq uniquify-after-kill-buffer-p t) ; rename after killing uniquified
+(setq uniquify-ignore-buffers-re "^\\*") ; don't muck with special buffers
