4d1d3d6799e0b40add22b5f7d3afc4a34d1eff32
Justin Lilly <justinlilly@gmail.com>
2008-10-22 09:46:54 -0400

    Altering generic structure to more defined areas of generic-ness. turned "yes or no" into "y or n" for exits showing time and date in modeline

--- configs/cfg_generic.el
+++ configs/cfg_generic.el
@@ -1,16 +1,21 @@
-(setq inhibit-startup-message t) ;; no splash screen
+;; ALTER DEFAULT BEHAVIOR
 (prefer-coding-system 'utf-8) ;; use UTF-8
 (setq backup-by-copying-when-linked t) ;; preserve hard links
 (setq backup-by-copying-when-mismatch t) ;; preserve owner:group
 (global-font-lock-mode 1) ;; make pretty fonts?
-
-(setq-default transient-mark-mode t) ;; shows current selected region
 (setq-default indent-tabs-mode nil) ;; indent via spaces not tabs
 (setq frame-title-format '(buffer-file-name "%f" ("%b"))) ;; titlebar = buffer unless filename
-(show-paren-mode 1) ;; show paired parenthasis
-(set-default-font
- "-apple-consolas-medium-r-normal--12-120-72-72-m-120-mac-roman")
 (toggle-debug-on-error t) ;; show traceback on error
+(fset 'yes-or-no-p 'y-or-n-p) ;; allows you to type "y" instead of "yes" on exit
+
+;; VISUAL DISPLAY
+(setq display-time-day-and-date t) ;; shows date in modeline 
+(display-time) ;; shows datetime in the mode line
+(setq inhibit-startup-message t) ;; no splash screen
 (tool-bar-mode -1) ;; hide the excess chrome
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
+(setq-default transient-mark-mode t) ;; shows current selected region
+(set-default-font
+ "-apple-consolas-medium-r-normal--12-120-72-72-m-120-mac-roman")
+(show-paren-mode 1) ;; show paired parenthasis
