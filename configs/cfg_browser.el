eee6b0eb4a82346af96edf293383981b584c3f36
Justin Lilly <justinlilly@gmail.com>
2008-11-05 18:24:30 -0500

    spacing fix

--- configs/cfg_browser.el
+++ configs/cfg_browser.el
@@ -2,3 +2,5 @@
 (setq browse-url-browser-function 'browse-url-firefox
           browse-url-new-window-flag  t
           browse-url-firefox-new-window-is-tab t)
+
+(require 'w3m-load) ;; turns on w3m browser
