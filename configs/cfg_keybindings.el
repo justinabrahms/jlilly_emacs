420c20611843fe43a66c0a90f24827806da23855
Justin Lilly <justinlilly@gmail.com>
2008-11-10 01:14:05 -0500

    Removing mark whole buffer binding (C-x h works well) and adding in C-w as backward-kill-word on yegge's suggestion. Also remapping something to kill-region (cut)

--- configs/cfg_keybindings.el
+++ configs/cfg_keybindings.el
@@ -3,6 +3,9 @@
 
 (global-set-key [(control tab)] 'ido-switch-buffer)
 
-(global-set-key [(control shift a)] 'mark-whole-buffer)
 (global-set-key "\C-x\C-m" 'execute-extended-command) ;; provides an alternative to M-x
 (global-set-key "\C-c\C-m" 'execute-extended-command) ;; ditto
+
+(global-set-key "\C-w" 'backward-kill-word) ;; delete an entire word
+(global-set-key "\C-x\C-k" 'kill-region) ;; alternative for old C-w
+(global-set-key "\C-c\C-k" 'kill-region) ;; copy for slippery fingers
