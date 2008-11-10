f070b77fe876c605ada72ef12cce31251f20f258
Justin Lilly <justinlilly@gmail.com>
2008-11-10 01:10:45 -0500

    Adding helpers for managing w3m including buffer renaming

--- configs/cfg_browser.el
+++ configs/cfg_browser.el
@@ -1,6 +1,12 @@
-;; http://www.emacswiki.org/cgi-bin/wiki/BrowseUrl
-(setq browse-url-browser-function 'browse-url-firefox
-          browse-url-new-window-flag  t
-          browse-url-firefox-new-window-is-tab t)
-
 (require 'w3m-load) ;; turns on w3m browser
+
+;; http://www.emacswiki.org/cgi-bin/wiki/BrowseUrl
+(setq browse-url-browser-function 'w3m-browse-url)
+(autoload 'w3m-browse-url "w3m" "Ask a WWW browser to show a URL." t)
+(setq w3m-use-cookies t) ;; enable cookie use. Mmm. Cookies.
+
+(setq w3m-use-title-buffer-name t) ;; set the buffer name to website title
+
+
+;; optional keyboard short-cut
+(global-set-key "\C-xm" 'browse-url-at-point)
