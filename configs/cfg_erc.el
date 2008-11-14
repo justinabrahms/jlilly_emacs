bf001c7f064cb3b13334f0f7ce0375683fa04b6a
Justin Lilly <justinlilly@gmail.com>
2008-11-14 16:30:43 -0500

    Adding in auto-truncating buffers for erc as well as auto-join functionaly. Also adding in auto query on pm

--- configs/cfg_erc.el
+++ configs/cfg_erc.el
@@ -1,1 +1,11 @@
-(setq erc-nick-uniquifier "_")
+(setq erc-nick-uniquifier "_") ;; append _ if nick is taken
+(setq erc-auto-query 'buffer) ;; add a whois when someone pms
+
+;; auto join some channels
+(setq erc-autojoin-channels-alist
+      '(("freenode.net" "#emacs" "#nethack" "#django-social" "#michat")))
+
+;; keep erc from eating ram by truncating chat logs
+(setq erc-max-buffer-size 20000)
+(defvar erc-insert-post-hook)
+(add-hook 'erc-insert-post-hook 'erc-truncate-buffer)
