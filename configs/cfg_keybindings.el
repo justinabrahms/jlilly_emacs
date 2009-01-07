1b53cfd2c4545e3749f11be32fbfc9fa24c4eaec
Justin Lilly <justinlilly@gmail.com>
2009-01-07 10:02:55 -0500

    Adding f7 keybinding to toggle linum-mode

--- configs/cfg_keybindings.el
+++ configs/cfg_keybindings.el
@@ -9,3 +9,4 @@
 (global-set-key "\C-w" 'backward-kill-word) ;; delete an entire word
 (global-set-key "\C-x\C-k" 'kill-region) ;; alternative for old C-w
 (global-set-key "\C-c\C-k" 'kill-region) ;; copy for slippery fingers
+(global-set-key [f7] 'linum-mode) ;; toggle linum mode 
