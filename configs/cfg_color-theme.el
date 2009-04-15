c7ea476fad0a03d9278aa2f09e6f2cea951f55e8
Justin Lilly <justinlilly@gmail.com>
2009-04-15 03:25:41 -0400

    Updating the theme to inkpot for another round. Digging how it looks on Ubuntu.

--- configs/cfg_color-theme.el
+++ configs/cfg_color-theme.el
@@ -1,5 +1,6 @@
 ;; Color Theme 
 (require 'color-theme) 
 (color-theme-initialize)
-;; (load-file "~/.emacs.d/vendor/themes/twilight/color-theme-twilight.el")
-(color-theme-blippblopp)
+(load-file "~/.emacs.d/vendor/themes/twilight/color-theme-twilight.el")
+(load-file "~/.emacs.d/themes/inkpot.el")
+(color-theme-inkpot)
