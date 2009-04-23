3b9103573fb55f9175531f189c13d21e338bf033
Justin Lilly <justinlilly@gmail.com>
2009-04-23 01:02:19 -0400

    Adding nifty keybindings.

--- configs/cfg_keybindings.el
+++ configs/cfg_keybindings.el
@@ -13,8 +13,16 @@
 ;; This is your old M-x.
 (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
 
+;The generic apropos (of any symbol) is MUCH more useful than apropos-command
+(global-set-key "\C-ha" 'apropos)
+
+;Suspend-frame is stupid
+(global-set-key "\C-z" 'ignore)
+(global-set-key "\C-x\C-z" 'ignore)
 
 (global-set-key "\C-w" 'backward-kill-word) ;; delete an entire word
 (global-set-key "\C-x\C-k" 'kill-region) ;; alternative for old C-w
 (global-set-key "\C-c\C-k" 'kill-region) ;; copy for slippery fingers
 (global-set-key [f7] 'linum-mode) ;; toggle linum mode 
+
+(global-set-key "\C-xg" 'magit-status)
