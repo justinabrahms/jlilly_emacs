79c040eb04096bcc735b8877b75f552c014b5126
Justin Lilly <justinlilly@gmail.com>
2009-06-30 07:55:00 -0400

    Changing path to js2-mode

--- configs/cfg_javascript.el
+++ configs/cfg_javascript.el
@@ -1,5 +1,5 @@
 ;; http://www.emacsblog.org/2008/04/04/package-faves-js2-mode/
 ;; http://steve-yegge.blogspot.com/2008/03/js2-mode-new-javascript-mode-for-emacs.html
-(setq load-path (cons "/home/jlilly/.elisp/packages/js2-mode/build" load-path))
+(setq load-path (cons "/home/jlilly/.emacs.d/packages/js2-mode/build" load-path))
 (autoload 'js2-mode "js2" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
