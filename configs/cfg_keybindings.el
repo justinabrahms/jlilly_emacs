3bb63c721e687764217b5614487e9864b4fbe964
Justin Lilly <justin@justinlilly.com>
2009-09-11 23:32:55 -0700

    Stealing gnewman's keybindings

--- configs/cfg_keybindings.el
+++ configs/cfg_keybindings.el
@@ -24,6 +24,7 @@
 (global-set-key "\C-w" 'backward-kill-word) ;; delete an entire word
 (global-set-key "\C-x\C-k" 'kill-region) ;; alternative for old C-w
 (global-set-key "\C-c\C-k" 'kill-region) ;; copy for slippery fingers
-(global-set-key [f7] 'linum-mode) ;; toggle linum mode 
+(global-set-key [f8] 'bury-buffer) ;; bury the buffer
+(global-set-key (kbd "C-;") 'ibuffer)
 
 (global-set-key "\C-xg" 'magit-status)
