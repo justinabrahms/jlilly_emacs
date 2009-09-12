fa0dc50668c78338f002aa1b70cd809872b7fa0e
Justin Lilly <justin@justinlilly.com>
2009-09-11 23:28:32 -0700

    cleaning up generic file.

--- configs/cfg_generic.el
+++ configs/cfg_generic.el
@@ -1,17 +1,21 @@
 ;; ALTER DEFAULT BEHAVIOR
 (prefer-coding-system 'utf-8) ;; use UTF-8
+
 (setq user-temporary-file-directory "/tmp/")
+
 (setq
  backup-by-copying t ;; fix for Transmitt to work
  backup-by-copying-when-linked t ;; preserve hard links
  backup-by-copying-when-mismatch t ;; preserve owner:group
  frame-title-format '(buffer-file-name "%f" ("%b")) ;; titlebar = buffer unless filename
- transient-mark-mode t
- save-place t
+ column-number-mode t
+ transient-mark-mode t ;; show selections
  indicate-empty-lines t
  color-theme-is-global t
  magit-auto-update t
  magit-collapse-threshold nil
+ save-place-file "~/saveplace" ;; keep my ~/ clean
+ truncate-partial-width-windows nil
 
  set-mark-command-repeat-pop t ; Mark-ring is navigable by typing C-u C-SPC and then repeating C-SPC forever
 
@@ -26,12 +30,13 @@
  version-control t ;; user numbers for backups
  delete-old-versions t ;; silently delete extra backup versions
 
+ default-tab-width 4 ;; a tab is 4 spaces
+
  ido-default-file-method 'selected-window
  ido-default-buffer-method ' selected-window
  ido-save-directory-list-file "~/.emacs.d/ido.last"
  ido-use-filename-at-point t
  ido-use-url-at-point t
- save-place t
 )
 
 
@@ -45,7 +50,9 @@
 (auto-compression-mode +1) ;; auto compress/decompress files
 (line-number-mode +1)
 (auto-fill-mode +1)
+(global-linum-mode +1) ;; give me some line numbers
 (icomplete-mode +1) ;; incremental minibuffer completion
+(global-font-lock-mode +1) ;; make pretty fonts?
 (when (fboundp 'shell-command-completion-mode)
   (shell-command-completion-mode +1))
 (when (fboundp 'savehist-mode)
@@ -89,9 +96,7 @@
 (when (fboundp 'ibuffer)
   (global-set-key (kbd "C-x C-b") 'ibuffer))
 (when (fboundp 'org-mode)
-  (add-to-list 'auto-mode-alist '("/TODO\\'" . org-mode)))
-
-
+ (add-to-list 'auto-mode-alist '("/TODO\\'" . org-mode)))
 
 (when (string= "mac" window-system)
   ;; Mac-Specific Settings
@@ -102,9 +107,12 @@
   (set-default-font
    "-unknown-ProggyCleanTT-normal-normal-normal-*-*-*-*-*-m-0-iso10646-1"))
 
-(setq-default truncate-lines t) ;; truncate lines, not wrap
-(setq-default indent-tabs-mode nil) ;; indent via spaces not tabs
-(global-font-lock-mode 1) ;; make pretty fonts?
+(setq-default
+ org-log-done "note" ;; prompt for an org note when marking things as done
+ truncate-lines t ;; truncate lines, not wrap
+ save-place t ;; enable save-place globally by default
+ indent-tabs-mode nil) ;; indent via spaces not tabs
+
 (toggle-debug-on-error t) ;; show traceback on error
 (fset 'yes-or-no-p 'y-or-n-p) ;; allows you to type "y" instead of "yes" on exit
 (mouse-avoidance-mode 'cat-and-mouse) ;; mouse jumps away when typing under it
@@ -126,24 +134,19 @@
 
 ;; One of Apps
 (require 'rst) ;; require ReST mode
-(require 'textmate) ;; defunkt's textmate.el
-(textmate-mode)
+;(require 'textmate) ;; defunkt's textmate.el
+;(textmate-mode)
 (require 'dpaste)
-(require 'magit)
-(require 'egg) ;; will need to choose between this one and magit, I think.
 (require 'jira)
 (winner-mode t) ;; turn on saved buffer configs
-;; DIRED
-(require 'dired-details) ;; hide useless permission info in dired
-(dired-details-install)
-(setq dired-details-hidden-string "")
 (require 'smooth-scrolling) ;; stop text from jumping on scroll.
 (require 'smex)
 (require 'column-marker)
 (require 'gist)
-(require 'lorem-ipsum)
-(require 'dired-single)
-(require 'ectags)
+(require 'browse-kill-ring)
+(require 'vernacular-time)
+(browse-kill-ring-default-keybindings)
+;; (require 'ectags)
 
 ;; VISUAL DISPLAY
 (display-time) ;; shows datetime in the mode line
@@ -176,20 +179,6 @@
 (global-set-key (kbd "C-+") 'ryan/increase-font-size)
 (global-set-key (kbd "C--") 'ryan/decrease-font-size)
 
-;; Functions for configuring window geometry and placement
-(defun smart-split ()
-  "Split the frame into 80-column sub-windows, and make sure no window has
-   fewer than 80 columns."
-  ; From http://hjiang.net/archives/253
-  (interactive)
-  (defun smart-split-helper (w)
-    "Helper function to split a given window into two, the first of which has 
-     80 columns."
-    (if (> (window-width w) (* 2 81))
-    (let ((w2 (split-window w 82 t)))
-      (smart-split-helper w2))))
-  (smart-split-helper nil))
-
 ;Reload .emacs on the fly
 (defun reload-dot-emacs()
   (interactive)
@@ -197,6 +186,11 @@
       (save-buffer(get-buffer ".emacs")))
   (load-file "~/.emacs.d/init.el")
   (message ".emacs reloaded successfully"))
+
+(setq initial-frame-alist '((top . 1)
+                            (left . 1)
+                            (width . 100)
+                            (height . 45)))
 
 ;; MISC CRAP
 (defalias 'qrr 'query-replace-regexp)
