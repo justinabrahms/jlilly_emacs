81058d9e5ea0114c38eefb79c5fd4f80bfed47a8
Justin Lilly <justin@justinlilly.com>
2009-09-11 23:32:11 -0700

    Adding magit and egg (I like egg a lot, though the font is a bit too large)

--- configs/cfg_git.el
+++ configs/cfg_git.el
@@ -4,3 +4,7 @@
 (require 'git)
 (autoload 'git-blame-mode "git-blame"
   "Minor mode for incremental blame for git" t)
+
+(require 'magit)
+(autoload 'magit-status "magit" nil t) ;; autoload magit-status for magit mode
+(require 'egg) ;; will need to choose between this one and magit, I think.
