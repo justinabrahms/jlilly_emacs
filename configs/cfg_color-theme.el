a6b1f8fa82d607f3d97413d7b23956c86c35f1dd
Justin Lilly <justin@justinlilly.com>
2010-01-12 21:24:19 -0500

    Notes on good color themes.

--- configs/cfg_color-theme.el
+++ configs/cfg_color-theme.el
@@ -9,4 +9,22 @@
 (load-file "~/.emacs.d/themes/vibrant-ink/color-theme-vibrant-ink.el")
 (load-file "~/.emacs.d/themes/inkpot.el")
 (load-file "~/.emacs.d/themes/brookstream/brookstream.el")
+
+; good color themes:
+; LightBG
+; - andreas
+; - blipp blopp
+; - high contrast
+;
+; DarkBG
+; - Arjen
+; - Billw
+; - calm forest
+; - charcoal black
+; - clairty and beauty
+; - cooper dark
+; - deep blue
+; - hober
+; - renegade
+
 (color-theme-vibrant-ink)
