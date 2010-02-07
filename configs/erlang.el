84296653e0fe393ca5fdf662b644a41b3fa6cbd3
Justin Lilly <justin@justinlilly.com>
2010-02-07 14:42:08 -0800

    Don't error out if erlang isn't installed

--- configs/erlang.el
+++ configs/erlang.el
@@ -1,4 +1,9 @@
-(setq load-path (cons "/usr/local/Cellar/erlang/R13B02-1/lib/erlang/lib/tools-2.6.4/emacs" load-path))
-(setq erlang-root-dir "/usr/local/Cellar/erlang/R13B02-1/")
-(setq exec-path (cons "/usr/local/Cellar/erlang/R13B02-1/bin" exec-path))
-(require 'erlang-start)
+;;; Needs a bit of work to properly set paths, but shouldn't error if
+;;; erlang isn't installed.
+
+(if (executable-find "erlang")
+    (cond
+     (setq load-path (cons "/usr/local/Cellar/erlang/R13B02-1/lib/erlang/lib/tools-2.6.4/emacs" load-path))
+     (setq erlang-root-dir "/usr/local/Cellar/erlang/R13B02-1/")
+     (setq exec-path (cons "/usr/local/Cellar/erlang/R13B02-1/bin" exec-path))
+     (require 'erlang-start)))
