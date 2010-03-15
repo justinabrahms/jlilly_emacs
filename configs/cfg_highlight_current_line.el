ddd83c9703b3954bea6b4cc18d886bcc19b840fd
Justin Lilly <justin@justinlilly.com>
2010-03-15 11:01:59 -0400

    Line highlights a bit lighter

--- configs/cfg_highlight_current_line.el
+++ configs/cfg_highlight_current_line.el
@@ -5,6 +5,6 @@
                                         ; saddle brown
 (cond (window-system
        (custom-set-faces
-        '(highlight-current-line-face ((t (:background "#eee")))))
+        '(highlight-current-line-face ((t (:background "#efefef")))))
        (custom-set-variables
         '(highlight-current-line-globally t nil (highlight-current-line)))))
