214927b892abdd9cb0333d6d79c2028e1a80dafb
Justin Lilly <justin@justinlilly.com>
2010-03-20 22:56:38 -0400

    Now I can open javascript files without an error.

--- configs/cfg_javascript.el
+++ configs/cfg_javascript.el
@@ -3,9 +3,6 @@
 (autoload 'espresso-mode "espresso" "Start espresso-mode" t)
 (add-to-list 'auto-mode-alist '("\\.js$" . espresso-mode))
 (add-to-list 'auto-mode-alist '("\\.json$" . espresso-mode))
-(add-hook 'espresso-mode-hook 'moz-minor-mode)
-(add-hook 'espresso-mode-hook 'esk-paredit-nonlisp)
-(add-hook 'espresso-mode-hook 'run-coding-hook)
 (add-hook 'espresso-mode-hook 'idle-highlight)
 (setq espresso-indent-level 2)
  
