c47fd2837fb2d918b08a4634fd3f768b955f45dd
Justin Lilly <justin@justinlilly.com>
2010-07-19 10:15:29 -0400

    js2 mode instead of espresso.

--- configs/cfg_javascript.el
+++ configs/cfg_javascript.el
@@ -1,18 +1,9 @@
 ;;; stolen from starter-kit-js.el
-(add-to-list 'auto-mode-alist '("\\.js$" . espresso-mode))
-(add-to-list 'auto-mode-alist '("\\.json$" . espresso-mode))
-(add-hook 'espresso-mode-hook 'idle-highlight)
-(setq espresso-indent-level 2)
-
-(eval-after-load 'espresso
-  '(progn
-     ;; fixes problem with pretty function font-lock
-     (define-key espresso-mode-map (kbd ",") 'self-insert-command)
-     (font-lock-add-keywords 'espresso-mode
-                             '(("\\<\\(FIX\\|TODO\\|FIXME\\|HACK\\|REFACTOR\\):"
-                                1 font-lock-warning-face t)))
-     (font-lock-add-keywords
-      'espresso-mode `(("\\(function *\\)("
-                        (0 (progn (compose-region (match-beginning 1)
-                                                  (match-end 1) "ƒ")
-                                  nil)))))))
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.json$" . js2-mode))
+(setq
+ js2-highlight-level 3
+ js2-basic-offset 4
+ js2-mirror-mode t
+ js2-bounce-indent-p t
+ js2-indent-on-enter-key t)
