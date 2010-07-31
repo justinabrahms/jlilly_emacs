c21234d77cd5a91430a0b195c6a3a48d93e11bd1
Justin Lilly <justin@justinlilly.com>
2010-07-31 13:44:44 -0400

    Espresso -> js2mode

--- configs/cfg_javascript.el
+++ configs/cfg_javascript.el
@@ -1,18 +1,19 @@
 ;;; stolen from starter-kit-js.el
-(add-to-list 'auto-mode-alist '("\\.js$" . espresso-mode))
-(add-to-list 'auto-mode-alist '("\\.json$" . espresso-mode))
-(add-hook 'espresso-mode-hook 'idle-highlight)
-(setq espresso-indent-level 2)
+(autoload 'js2-mode "js2-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.json$" . js2-mode))
+(add-hook 'js2-mode-hook 'idle-highlight)
+(setq js2-basic-offset 2)
 
-(eval-after-load 'espresso
+(eval-after-load 'js2-mode
   '(progn
      ;; fixes problem with pretty function font-lock
-     (define-key espresso-mode-map (kbd ",") 'self-insert-command)
-     (font-lock-add-keywords 'espresso-mode
+     (define-key js2-mode-map (kbd ",") 'self-insert-command)
+     (font-lock-add-keywords 'js2-mode
                              '(("\\<\\(FIX\\|TODO\\|FIXME\\|HACK\\|REFACTOR\\):"
                                 1 font-lock-warning-face t)))
      (font-lock-add-keywords
-      'espresso-mode `(("\\(function *\\)("
+      'js2-mode `(("\\(function *\\)("
                         (0 (progn (compose-region (match-beginning 1)
                                                   (match-end 1) "ƒ")
                                   nil)))))))
