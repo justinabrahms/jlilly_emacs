8fb6b0c374d810ab08606de1ad717fa76afae80f
Justin Lilly <justin@justinlilly.com>
2010-08-04 06:42:49 -0400

    Adding django yasnippets to snippets directory

--- configs/cfg_snippets.el
+++ configs/cfg_snippets.el
@@ -3,5 +3,6 @@
 (yas/initialize)
 (setq yas/root-directory "~/.emacs.d/snippets")
 (loop for dir in '("~/.emacs.d/snippets"
-                   "~/.emacs.d/vendor/yasnippet/snippets")
+                   "~/.emacs.d/vendor/yasnippet/snippets"
+                   "~/.emacs.d/vendor/yasnippet-django/")
       do (yas/load-directory dir))
