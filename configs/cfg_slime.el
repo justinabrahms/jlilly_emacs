53e1df7999b2b99ea0ade325c3cb8764a250d25b
Justin Lilly <justin@justinlilly.com>
2010-08-10 17:05:34 -0400

    Conditionally load slime mode depending on the presence of sbcl on PATH

--- configs/cfg_slime.el
+++ configs/cfg_slime.el
@@ -1,9 +1,9 @@
-;; should be able to set this in a conditional based around (executable-find), on an error
-(eval-after-load "slime"
-  '(progn (slime-setup '(slime-repl))))
+(if (executable-find "sbcl")
+    (progn
+      (eval-after-load "slime"
+	'(progn (slime-setup '(slime-repl))))
 
-(require 'slime)
-(setq inferior-lisp-program (or
-                             (executable-find "sbcl")
-                             "/opt/local/bin/sbcl"))
-(slime-setup)
+
+      (require 'slime)
+      (setq inferior-lisp-program (executable-find "sbcl"))
+      (slime-setup)))
